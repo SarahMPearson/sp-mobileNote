8d98fd8605c0e4b8177322b9ef01499d34834177
Sarah Pearson <sarahp@me.com>
2014-12-09 11:32:57 -0600

    login form

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,17 +1,17 @@
-'use strict';
+(function(){
+  'use strict';
 
-angular.module('starter.controllers', [])
+  angular.module('starter')
 
-.controller('DashCtrl', function($scope){
-})
+  .controller('DashCtrl', function($scope){
+  })
 
-.controller('FriendsCtrl', function($scope, Friends){
-  $scope.friends = Friends.all();
-})
+  .controller('FriendsCtrl', function($scope, Friends){
+    $scope.friends = Friends.all();
+  })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends){
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+  .controller('FriendDetailCtrl', function($scope, $stateParams, Friends){
+    $scope.friend = Friends.get($stateParams.friendId);
+  });
 
-.controller('AccountCtrl', function($scope){
-});
+})();
---
+++ www/js/controllers/account-controller.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict';
+
+  angular.module('starter')
+  .controller('AccountCtrl', function($scope){
+    $scope.user = {};
+    $scope.login = function(user){
+      console.log('user', user);
+    };
+  });
+
+})();
